282ddb7f560e7b4ec53b1767d95bfd3a56c82a4b
Dmytro Diukov <diukovdmitriy@gmail.com>
2023-09-24 11:25:19 +0300

    char load functional

--- src/index.js
+++ src/index.js
@@ -9,7 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-//todo:
-//if description is empty, show message 'There is no description for this character'
-//if description is too long, show only first 210 symbols and add '...'
